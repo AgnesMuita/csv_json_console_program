f6d8156112deb82a3790f76ef804f05190a6b6ba
AgnesMuita <wamwithamuita@gmail.com>
2022-11-02 23:31:34 +0300

    append sha256 to csv line

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 import csv
 import json
 import hashlib
-from csv import writer
+import pandas as pd
+
 
  
 #convert csv to json
@@ -29,3 +30,11 @@ with open(filename,'rb')as f:
   for byte_block in iter(lambda: f.read(4096),b""):
     sha256_hash.update(byte_block)
   print (sha256_hash.hexdigest())
+
+#append sha256 to csv line
+df = pd.read_csv("NFT_CSV_file_csv.csv")
+df["sha256"] = (sha256_hash.hexdigest())
+df.to_csv("filename.csv", index=False)
+
+
+
