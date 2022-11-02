bdd32bd2ad2447bf428c144b8735f51c03a64dc1
AgnesMuita <wamwithamuita@gmail.com>
2022-11-02 22:31:54 +0300

    optimize hashing function to handle large files

--- script.py
+++ script.py
@@ -23,7 +23,8 @@ make_json(csvFilePath,jsonFilePath)
 
 #calculate sha256
 filename = "CHIP-0007.json"
+sha256_hash = hashlib.sha256()
 with open(filename,'rb')as f:
-  bytes = f.read()
-  readable_hash=hashlib.sha256(bytes).hexdigest()
-  print (readable_hash)
+  for byte_block in iter(lambda: f.read(4096),b""):
+    sha256_hash.update(byte_block)
+  print (sha256_hash.hexdigest())
