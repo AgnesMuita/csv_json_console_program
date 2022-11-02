adc7aef4d6b8481d70ef1c7a17cd6feff3df217e
AgnesMuita <wamwithamuita@gmail.com>
2022-11-02 22:26:06 +0300

    calculate json SHA256

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import hashlib
 
  
 #convert csv to json
@@ -20,4 +21,9 @@ jsonFilePath=input('Enter the absolute path of the json file')
 
 make_json(csvFilePath,jsonFilePath)
 
-
+#calculate sha256
+filename = "CHIP-0007.json"
+with open(filename,'rb')as f:
+  bytes = f.read()
+  readable_hash=hashlib.sha256(bytes).hexdigest()
+  print (readable_hash)
