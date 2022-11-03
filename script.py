6633e04b18813560584548da461d79ffafc3c4d8
AgnesMuita <wamwithamuita@gmail.com>
2022-11-03 16:28:27 +0300

    clean up

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ import pandas as pd
 from pathlib import Path
 import os
 
-
+#create a directory to store the generate the json files
 def create_directory(directory):
   parent_dir = os.getcwd()
   if not os.path.exists("nftjsonfiles"):
@@ -15,6 +15,7 @@ def create_directory(directory):
 create_directory("nftjsonfiles")
 
 
+#convert csv rows to json
 def make_json(csvFilePath):
     jsonFilePath=os.getcwd() + "\\nftjsonfiles"
 
@@ -31,26 +32,26 @@ def make_json(csvFilePath):
 
 
 csvFilePath=input('Enter the absolute path of the csv file')
-
-
 make_json(csvFilePath)
 
 
-# calculate sha256
-FilePath = os.getcwd() + "\\nftjsonfiles"
-for filename in os.listdir(FilePath):
-  f=os.path.join(FilePath, filename)
-  if os.path.isfile(f):
-    sha256_hash = hashlib.sha256()
-    with open(f,'rb')as f:
-      for byte_block in iter(lambda: f.read(4096),b""):
-        sha256_hash.update(byte_block)
-        print (sha256_hash.hexdigest())
+# calculate sha256 for each json file and append
+def calculate_hash():
+  FilePath = os.getcwd() + "\\nftjsonfiles"
+  for filename in os.listdir(FilePath):
+    f=os.path.join(FilePath, filename)
+    if os.path.isfile(f):
+      sha256_hash = hashlib.sha256()
+      with open(f,'rb')as f:
+        for byte_block in iter(lambda: f.read(4096),b""):
+          sha256_hash.update(byte_block)
+          print (sha256_hash.hexdigest())
 
-    # append sha256 to csv line
-    df = pd.read_csv(csvFilePath)
-    df["sha256"] = (sha256_hash.hexdigest())
-    df.to_csv(csvFilePath+ ".output.csv", index=False)
+      # append sha256 to csv line
+      df = pd.read_csv(csvFilePath)
+      df["sha256"] = (sha256_hash.hexdigest())
+      df.to_csv(csvFilePath+ ".output.csv", index=False)
+calculate_hash()
 
 
 
