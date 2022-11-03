d497aeb4a2779b1ee58a686d2c4da4b9a644eec9
AgnesMuita <wamwithamuita@gmail.com>
2022-11-03 23:58:20 +0300

    update append to csv file function

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+from calendar import c
 import copy
 import csv
 import json
@@ -6,7 +7,7 @@ import pandas as pd
 from pathlib import Path
 import os
 
-#create a directory to store the generate the json files
+#create a directory to store the generated  json files
 def create_directory(directory):
   parent_dir = os.getcwd()
   if not os.path.exists("nftjsonfiles"):
@@ -20,23 +21,26 @@ def make_json(csvFilePath):
     jsonFilePath=os.getcwd() + "\\nftjsonfiles"
 
     with open(csvFilePath, encoding='utf-8') as csvf:
-      csvReader = csv.DictReader(csvf, fieldnames=("No","Name","Hash","UUID"))
+      csvReader = csv.DictReader(csvf, fieldnames=("format",'TEAM NAMES', "Series Number",'Filename','Description','Gender','attributes', "UUID"))
       for row in csvReader:
+        row["format"] = "CHIP-0007"
+        # row='{"collection":"Example"}'
         out=json.dumps(row, indent=4)
         n=json.loads(out)
-        if n['No'] != 'No':
-          jsonoutput = open(jsonFilePath+'\\'+str(n['No'])+'.json','w')
+        if n['Filename'] != 'Filename':
+          jsonoutput = open(jsonFilePath+'\\'+str(n['Filename'])+'.json','w')
           jsonoutput.write(out)
           jsonoutput.close()
       csvf.close()
 
 
-csvFilePath=input('Enter the absolute path of the csv file')
+csvFilePath = os.getcwd()+'\\'+ input('enter the name of csv file ')
 make_json(csvFilePath)
 
 
 # calculate sha256 for each json file and append
 def calculate_hash():
+  hashList = []
   FilePath = os.getcwd() + "\\nftjsonfiles"
   for filename in os.listdir(FilePath):
     f=os.path.join(FilePath, filename)
@@ -45,14 +49,17 @@ def calculate_hash():
       with open(f,'rb')as f:
         for byte_block in iter(lambda: f.read(4096),b""):
           sha256_hash.update(byte_block)
+          hashes = sha256_hash.hexdigest()
+          hashList.append(hashes)
 
-      # append sha256 to csv line
-      df = pd.read_csv(csvFilePath)
-      df["sha256"] = (sha256_hash.hexdigest())
-      df.to_csv(csvFilePath+ ".output.csv", index=False)
+        # append sha256 to csv line
+      for hash in hashList:
+        df = pd.read_csv(csvFilePath)
+        df["sha256"] = hash
+        fname=Path(csvFilePath).stem
+        df.to_csv(fname+ ".output.csv", index=False)
 
 calculate_hash()
-
 
 
 
