2eb5bf4c8e378d5a37729ecfa513a8b9d423f867
AgnesMuita <wamwithamuita@gmail.com>
2022-11-14 19:53:42 +0300

    update CHIP-0007 json

--- script.py
+++ script.py
@@ -2,74 +2,98 @@ import csv
 import hashlib
 import json
 import os
-
-create_csv = 'HNGi9 CSV FILE - Sheet1.output.csv'
+# This is creating a csv file and writing the header to it.
+create_csv = 'filename.csv'
 f = open(create_csv, 'w')
 writer = csv.writer(f)
+# create new directory to store the generated json
 
-#create new directory to store the generated json
+
 def create_directory(directory):
   parent_dir = os.getcwd()
   if not os.path.exists("nftjsonfiles"):
     path = os.path.join(parent_dir, directory)
     os.mkdir(path)
+
+
 create_directory("nftjsonfiles")
 
-#convert csv to json
-def csv_to_json():
-  with open('HNGi9 CSV FILE - Sheet1.csv', 'r') as csvf:
-      read_csv = csv.reader(csvf, delimiter=',')
-      next(read_csv)
-      csvReader = [a for a in read_csv]
-    # create a json file for each row in the csv file.
+
+def make_json():
+    csvFilePath = "HNGi9 CSV FILE - Sheet1.csv"
+    jsonFilePath = os.getcwd() + "\\nftjsonfiles"
+
+    with open(csvFilePath, encoding='utf-8') as csvf:
+      csvReader = csv.DictReader(csvf, fieldnames=(
+          'TEAM NAMES', "Series Number", 'Filename', 'Name', 'Description', 'Gender', 'attributes', "UUID"))
       for row in csvReader:
-        if row[1] and row[2]:
-            series_number = row[0]
-            file_name = row[1]
-            name = row[2]
-            description = row[3]
-            gender = row[4]
-            attributes = row[5]
-            uuid = row[6]
+        out = json.dumps(row, indent=4)
+        n = json.loads(out)
+        if n['Filename'] != 'Filename':
+          jsonoutput = open(jsonFilePath+'\\'+str(n['Filename'])+'.json', 'w')
+          jsonoutput.write(out)
+          jsonoutput.close()
+      csvf.close()
 
-            json_file = {
-                'format': 'CHIP-0007',
-                'name': file_name.replace('-', ' ').title(),
-                'description': description,
-                'minting_tool': '',
-                'series_number': series_number,
-                'sensitive_content': False,
-                'series_total': csvReader[-1][0],
-                "attributes": [
-                    {
-                        "trait_type": "gender",
-                        "value": gender
-                    }
-                ],
-                "collection": {
-                    "name": "Zuri NFT tickets for free lunch",
-                    "id": uuid,
-                    "attributes": [
-                        {
-                            "type": "description",
-                            "value": "Rewards for accomplishments during HNGi9"
-                        }
-                    ]
-                },
-            }
 
-            # Convert the json file to a string.
-            jsonoutput = json.dumps(json_file, indent=4)
-            with open(f'nftjsonfiles/{name}.json', 'w') as output:
-                output.write(jsonoutput)
-            output.close()
+make_json()
 
-          # Create a hash of the json file and append it to the csv file.
-            hashString = hashlib.sha256(jsonoutput.encode()).hexdigest()
-            row.append(hashString)
-            writer.writerow(row)
+FilePath = os.getcwd() + "\\nftjsonfiles"
+for filename in os.listdir(FilePath):
+    f = os.path.join(FilePath, filename)
+    with open(f) as json_output:
+      csvReader = json_output.read()
+      for row in csvReader:
+        print(row)
 
-  # Closing the file.
-  f.close()
+# Reading the csv file and skipping the first row.
+with open(csvReader, encoding='utf-8') as csv_file:
+    read_csv = csv.reader(csv_file, delimiter=',')
+    # next(read_csv)
+    csvRead = [a for a in read_csv]
+   # Creating a json file for each row in the csv file.
+for row in read_csv:
+    # print(row)
+    currentteamname = ""
+    outerkeycounter = 0
+    seriestotal = len(csvReader)
+    itemobject = {'format': 'CHIP-0007',
+                  "sensitive_content": False, "series_total": seriestotal}
+    for innerkey in csvReader:
+        if innerkey == "attributes":
+            itemobject["attributes"] = []
+            existingattributes = innerkey['attributes'].split(";")
+            for attribute in existingattributes:
+                attributeproperties = attribute.split(":")
+                if len(attributeproperties) == 2 and attributeproperties[0] and attributeproperties[1]:
+                    newattribute = {
+                        "trait_type": attributeproperties[0], "value": attributeproperties[1]}
+                    itemobject['attributes'].append(newattribute)
+        elif innerkey == 'TEAM NAMES':
+            itemobject['minting_tool'] = currentteamname
+        elif innerkey == "UUID":
+            itemobject['collection'] = {"name": "Zuri NFT Tickets for Free Lunch", "id": row['UUID'], "attributes": [{
+                "type": "description",
+                "value": "Rewards for accomplishments during HNGi9."
+            }]}
+        # else:
+            # pass
+            # itemobject[row] = csvReader
+        outerkeycounter += 1
 
-csv_to_json()
+    # Creating a json file for each row in the csv file.
+    # Converting the json file to a string.
+    jsonObject = json.dumps(itemobject, indent=4)
+    print(jsonObject)
+    with open(f'nftjsonfiles/{jsonObject.format}.json', 'w') as output:
+        output.write(jsonObject)
+    output.close()
+
+    # Creating a hash of the json file and appending it to the csv file.
+    hashString = hashlib.sha256(jsonObject.encode()).hexdigest()
+    # Appending the file name to the csv file.
+    row.append(hashString)
+    writer.writerow(row)
+
+# Closing the file.
+f.close()
